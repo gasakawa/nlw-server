45726fc3e2a4c0c6aa979a49b070490ef2c8e627
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-02 15:10:27 -0300

    chore: configure database connection and run migrations

---
+++ knexfile.ts
@@ -0,0 +1,14 @@
+// Update with your config settings.
+
+import path from 'path';
+
+module.exports = {
+  client: 'sqlite3',
+  connection: {
+    filename: path.resolve(__dirname, 'src', 'database', 'database.sqlite'),
+  },
+  migrations: {
+    directory: path.resolve(__dirname, 'src', 'database', 'migrations'),
+  },
+  useNullAsDefault: false,
+};

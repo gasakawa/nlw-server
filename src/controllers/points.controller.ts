cf533c0b7271083e7782ff56309e993fa2213927
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-02 18:41:49 -0300

    feat: save point with fixed image

--- src/controllers/points.controller.ts
+++ src/controllers/points.controller.ts
@@ -15,7 +15,8 @@ export default class PointsController {
         lng,
         city,
         uf,
-        image: 'image-fake',
+        image:
+          'https://images.unsplash.com/photo-1542838132-92c53300491e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&q=60',
       };
 
       const [insertedId] = await trx('points').insert(point);

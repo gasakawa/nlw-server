1990973ecf4111cae0e06af5a6df0e9d7bb4fc8c
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-07 07:43:52 -0300

    feat: add validation data to point's create route

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import multer from 'multer';
 import multerConfig from '../config/multer';
+import { celebrate, Joi } from 'celebrate';
 
 import PointsController from '../controllers/points.controller';
 import ItemsController from '../controllers/items.controller';
@@ -12,7 +13,28 @@ const itemsController = new ItemsController();
 
 routes.get('/items', itemsController.index);
 
-routes.post('/points', upload.single('image'), poinstController.create);
+routes.post(
+  '/points',
+  upload.single('image'),
+  celebrate({
+    body: Joi.object().keys(
+      {
+        name: Joi.string().required(),
+        email: Joi.string().required(),
+        whatsapp: Joi.string().required(),
+        lat: Joi.number().required(),
+        lng: Joi.number().required(),
+        city: Joi.string().required(),
+        uf: Joi.string().required().max(2),
+        items: Joi.string().required(),
+      },
+      {
+        abortEarly: false,
+      },
+    ),
+  }),
+  poinstController.create,
+);
 
 routes.get('/points', poinstController.index);
 
